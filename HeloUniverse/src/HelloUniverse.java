4f5de219420b234f1d11361a5a7a37be95e94efe
melissa98m <melissa.mangione@gmail.com>
2022-02-16 17:36:47 +0100

    boucle for

--- HeloUniverse/src/HelloUniverse.java
+++ HeloUniverse/src/HelloUniverse.java
@@ -1,20 +1,20 @@
 public class HelloUniverse {
     public static void main(String... args) {
 
-        int nbPlanets = 1;
-        switch (nbPlanets) {
-            case 7:
-                System.out.println("On sait qu'au 16ème siècle, seules 7 planètes avaient été découvertes");
-                break;
-            case 8:
-                System.out.println("On sait que le nombre de planètes est passé de 7 à 8 au 17ème siècle, mais il a également été réduit de 9 à 8 en 2006");
-                break;
-            case 9:
-                System.out.println("On sait que le nombre de planètes est passé de 8 à 9 au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8");
-                break;
-            default:
-                System.out.printf("Le programme ne peut pas fournir de résultat pour %d", nbPlanets);
+        for (int nbPlanets = 7;nbPlanets<= 9; nbPlanets++) {
+            switch (nbPlanets) {
+                case 7:
+                    System.out.println("On sait qu'au 16ème siècle, seules 7 planètes avaient été découvertes");
+                    break;
+                case 8:
+                    System.out.println("On sait que le nombre de planètes est passé de 7 à 8 au 17ème siècle, mais il a également été réduit de 9 à 8 en 2006");
+                    break;
+                case 9:
+                    System.out.println("On sait que le nombre de planètes est passé de 8 à 9 au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8");
+                    break;
+                default:
+                    System.out.printf("Le programme ne peut pas fournir de résultat pour %d", nbPlanets);
+            }
         }
     }
-
 }
