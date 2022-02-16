950b570a5bc3940c29a4788e01d22e10c8127ccc
melissa98m <melissa.mangione@gmail.com>
2022-02-16 17:17:46 +0100

    condition if else + switch

--- HeloUniverse/src/HelloUniverse.java
+++ HeloUniverse/src/HelloUniverse.java
@@ -1,26 +1,20 @@
 public class HelloUniverse {
     public static void main(String... args) {
-       short year = 2006;
-        int nbPlanets = 0;
-        String message = "En %d ,  les planètes du système solaire étaient au nombre de : %d";
-        if (year < 1600 || year > 2022) {
-            System.out.printf("Le programme ne peut donné un resultat car l'année %d saisie n'est pas valide" , year);
+
+        int nbPlanets = 1;
+        switch (nbPlanets) {
+            case 7:
+                System.out.println("On sait qu'au 16ème siècle, seules 7 planètes avaient été découvertes");
+                break;
+            case 8:
+                System.out.println("On sait que le nombre de planètes est passé de 7 à 8 au 17ème siècle, mais il a également été réduit de 9 à 8 en 2006");
+                break;
+            case 9:
+                System.out.println("On sait que le nombre de planètes est passé de 8 à 9 au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8");
+                break;
+            default:
+                System.out.printf("Le programme ne peut pas fournir de résultat pour %d", nbPlanets);
         }
-        else
-        {
-            if(year < 1700){
-                nbPlanets=7;
-            }
-            else if(year < 1800){
-                nbPlanets=8;
-            }
-            else if(year < 2006){
-                nbPlanets=9;
-            }
-            else {
-                nbPlanets=8;
-            }
-        }
-        System.out.printf(message , year, nbPlanets);
     }
+
 }
