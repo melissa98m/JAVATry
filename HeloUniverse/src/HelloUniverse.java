bf010ec9da3baf91ddb72c082220877524825d69
melissa98m <melissa.mangione@gmail.com>
2022-02-28 08:32:36 +0100

    class scanner

--- HeloUniverse/src/HelloUniverse.java
+++ HeloUniverse/src/HelloUniverse.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class HelloUniverse {
     public static void main(String... args) {
 
@@ -22,9 +24,9 @@ public class HelloUniverse {
         Neptune.diameter = 49532;
         Neptune.revolution(-3543);
 
-        System.out.printf("%s est une planete  avec un diametre de %d", jupiter.name,jupiter.diameter);
+        System.out.printf("%s est une planete  avec un diametre de %d", jupiter.name, jupiter.diameter);
         PlaneteTellurique Sedna = new PlaneteTellurique("Sedna");
-        System.out.printf("%s est une planete  avec un diametre de %d", Sedna.name,Sedna.diameter);
+        System.out.printf("%s est une planete  avec un diametre de %d", Sedna.name, Sedna.diameter);
         System.out.println("Neptune a effectué " + Neptune.revolution(-3442) + " tours autour de son étoile");
         System.out.println("Mars a effectué " + Mars.rotation(-684) + " tours sur elle même");
         System.out.println("Vénus a effectué " + Venus.rotation(1250) + " tours sur elle même");
@@ -60,33 +62,49 @@ public class HelloUniverse {
         Vaisseau chasseur = new VaisseauDeGuerre("Chasseur");  //polymorphysme
         chasseur.type = "Chasseur";
         chasseur.blindage = 156;
-        chasseur.resistanceDuBouclier=2;
+        chasseur.resistanceDuBouclier = 2;
 
 
         Vaisseau vaisseauMonde = new VaisseauCivil("Vaisseau-Monde");
-        vaisseauMonde.blindage= 4784;
-        vaisseauMonde.resistanceDuBouclier=30;
+        vaisseauMonde.blindage = 4784;
+        vaisseauMonde.resistanceDuBouclier = 30;
         vaisseauMonde.activerBouclier();
 
         chasseur.activerBouclier();
-        ((VaisseauDeGuerre) chasseur).attaquer(vaisseauMonde,"lasers photoniques" , 30 );
+        ((VaisseauDeGuerre) chasseur).attaquer(vaisseauMonde, "lasers photoniques", 30);
 
         //exemple de transtypage
         Vaisseau croiser = new VaisseauDeGuerre("Croiser");
         croiser.type = "Croiser";
-        ((VaisseauDeGuerre)croiser).attaquer(chasseur , "Lasers violet" , 5);
+        ((VaisseauDeGuerre) croiser).attaquer(chasseur, "Lasers violet", 5);
 
 
         vaisseauMonde.desactiverBouclier();
         System.out.println("Resistance du boublier du VM est " + vaisseauMonde.resistanceDuBouclier);
-        System.out.println("Le blindage du VM est "+ vaisseauMonde.blindage);
+        System.out.println("Le blindage du VM est " + vaisseauMonde.blindage);
         Mars.accueillirVaisseau(vaisseauMonde);
-       Mars.accueillirVaisseau(chasseur);
+        Mars.accueillirVaisseau(chasseur);
 
 
-       VaisseauDeGuerre newChasseur = new VaisseauDeGuerre("Chasseur");
-       Terre.accueillirVaisseau(newChasseur);
-       System.out.println("Le chasseur a refusée"+ newChasseur.emporterCargaison(20) +" tonnes");
+        VaisseauDeGuerre newChasseur = new VaisseauDeGuerre("Chasseur");
+        Terre.accueillirVaisseau(newChasseur);
+        System.out.println("Le chasseur a refusée" + newChasseur.emporterCargaison(20) + " tonnes");
+      /* System.out.println(System.getProperties()); // permet de connaitre les propriété
+         System.err.println("error") //print une erreur
+         System.in.println() //permet de savoir ce que l'utilisateur a entrer au clavier
+         System.exit(int) // code de retour du programme si normal 0 (met fin a la machine virtuelle) , si 1 code erreur
+         long time =System.currentTimeMilis() //permet de retourner le nombre de milliseconde depuis la machine virtuelle
+         System.nanoTime() //permet de savoir combien de temps s'execute une commande un System.nanTime avant la commande et une apres
+       */
+
+        Scanner sc=new Scanner(System.in);
+        String ligneNext= sc.nextLine(); // renvoit la ligne taper dans le terminale
+        System.out.println("La ligne suivante est " + ligneNext);
+
+        Scanner nc=new Scanner(System.in);
+        int entierSuivant=nc.nextInt();//renvoit l'entier taper
+        System.out.println("L' entier suivant est " + entierSuivant);
+
     }
 
 }
